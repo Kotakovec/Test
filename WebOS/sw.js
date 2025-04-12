da33cb955e9704a6760c3ba7e23896c0828959ed
Kotakovec <kotakjakub@gmail.com>
2025-04-12 13:52:35 +0200

    Update sw.js

--- WebOS/sw.js
+++ WebOS/sw.js
@@ -1,26 +1,22 @@
-const CACHE_NAME = 'webos-cache-v1';
+const CACHE_NAME = 'pokracovat-app-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/icon.png'
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
